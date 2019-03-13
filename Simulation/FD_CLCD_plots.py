9a5a17773eda1affb3b4979832b49c6ba6aeea9e
tommyvtran97 <48211985+tommyvtran97@users.noreply.github.com>
2019-03-13 21:42:54 +0100

    Add files via upload

---
+++ Simulation/FD_CLCD_plots.py
@@ -0,0 +1,42 @@
+from FD_CLCD import *
+
+'*** CL-Alpha Plot ***'
+#z = np.polyfit(alpha, CL, 1)
+#p1 = np.poly1d(z)
+#plt.plot(alpha, p1(alpha),"b--")
+#plt.plot(alpha, CL, 'x')
+#plt.xlabel('alpha [deg]')
+#plt.ylabel('CL [-]')
+
+'*** CD-Alpha Plot ***'
+#z = np.polyfit(alpha, CD, 2)
+#p1 = np.poly1d(z)
+#plt.plot(alpha, p1(alpha),"b--") 
+#plt.plot(alpha, CD, 'x')
+#plt.xlabel('alpha [deg]')
+#plt.ylabel('CD [-]')
+#plt.show()
+
+'*** CL-CD Plot [approximate] ***'
+#z = np.polyfit(CD, CL, 2)
+#p1 = np.poly1d(z)
+#plt.plot(CD, p1(CD),"b--")  
+#plt.plot(CD, CL, 'x')
+#plt.xlabel('CD [-]')
+#plt.ylabel('CL [-]')
+
+'*** CD-CL^2 Plot ***'
+#z = np.polyfit(CL_2, CD, 1)
+#p1 = np.poly1d(z)
+#plt.plot(CL_2, p1(CL_2))
+#plt.plot(CL_2, CD, 'x')
+
+'*** CL-CD plot [accurate] ***'
+CL_new = np.arange(-0.3, 1.4, 0.02)
+CD_new = []
+
+for c in range(len((CL_new))):
+    CD_new.append(CD0 + (CL_new[c]**2)/(pi*A*e))
+    
+plt.plot(CD_new, CL_new, '^', markersize=2.0)
+
