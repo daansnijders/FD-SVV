c91473c28c84384c3effa1bbf675407fb49602ca
Daan Snijders <daan.97@hotmail.com>
2019-03-13 11:53:03 +0100

    Update statespace.py

--- Simulation/statespace.py
+++ Simulation/statespace.py
@@ -6,18 +6,20 @@ from scipy import signal
 
 """Symmetric equations of motion to state space form"""
 
+V_test = 1.
+
 delta_e = 1.
+
 u = 1.
 alpha = 1.
 theta = 2. 
 q = 3.
-V_test = 1.
+
 u_dot = 1.
 alpha_dot = 1.
 theta_dot = 1.
 q_dot = 1.
 
-Dc = c / V_test 
 
 u_bar_sym = np.array([delta_e])
 x_bar_sym = np.array([[u], [alpha], [theta], [q]])
@@ -28,30 +30,32 @@ C2_sym = np.array([[CXu/V_test, CXa, CZ0, c/V_test*CXq], [CZu/V_test, CZa, -CX0,
 C3_sym = np.array([[CXde], [CZde], [0], [Cmde]])
 
 
-A = -np.matmul(np.linalg.inv(C1_sym), C2_sym)
-B = -np.matmul(np.linalg.inv(C1_sym), C3_sym)
-C = np.array([[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]])
-D = np.array([[0],[0],[0],[0]])
+A_sym = -np.matmul(np.linalg.inv(C1_sym), C2_sym)
+B_sym = -np.matmul(np.linalg.inv(C1_sym), C3_sym)
+C_sym = np.array([[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]])
+D_sym = np.array([[0],[0],[0],[0]])
 
-sys_sym = signal.StateSpace(A, B, C, D)
+sys_sym = signal.StateSpace(A_sym, B_sym, C_sym, D_sym)
 
 print (sys_sym)
 
 """#Asymmetric equations of motion in state space form"""
 
+V_test = 1.
+
 delta_a = 1.
 delta_r = 1.
-u = 1.
-alpha = 1.
-theta = 2. 
-q = 3.
-V_test = 1.
-u_dot = 1.
-alpha_dot = 1.
-theta_dot = 1.
-q_dot = 1.
 
-Dc = c / V_test 
+beta = 1.
+phi = 2. 
+p = 3.
+r = 4.
+
+beta_dot = 1.
+phi_dot = 1.
+p_dot = 1.
+r_dot = 1.
+
 
 u_bar_sym = np.array([[delta_a], [delta_r]])
 x_bar_sym = np.array([[beta], [phi], [p], [r]])
@@ -59,17 +63,17 @@ x_bardot_sym = np.array([[beta_dot], [phi_dot], [p_dot], [r_dot]])
 
 C1_asym = np.array([[(CYbdot-2*mub)*b/V_test, 0, 0, 0],[0, -1/2*b/V_test, 0, 0],[0, 0, -4*mub*KX2*b/V_test*b/2/V_test, 4*mub*KXZ*b/V_test*b/2/V_test],[Cnbdot*b/V_test, 0, 4*mub*KXZ*b/V_test*b/2/V_test, -4*mub*KZ2*b/V_test*b/2/V_test]])
 C2_asym = np.array([[CYb, CL, CYp*b/2/V_test, (CYr-4*mub)*b/2/V_test],[0,0,1*b/2/V_test,0],[Clb, 0, Clp*b/2/V_test, Clr*b/2/V_test],[Cnb, 0, Cnp*b/2/V_test, Cnr*b/2/V_test]])
-C3_asym = np.array([[CYda, Cydr], [0,0], [Clda, Cldr], [Cnda, Cndr]])
+C3_asym = np.array([[CYda, CYdr], [0,0], [Clda, Cldr], [Cnda, Cndr]])
 
 
-A = -np.matmul(np.linalg.inv(C1_asym), C2_asym)
-B = -np.matmul(np.linalg.inv(C1_asym), C3_asym)
-C = np.array([[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]])
-D = np.array([[0,0],[0,0],[0,0],[0,0]])
+A_asym = -np.matmul(np.linalg.inv(C1_asym), C2_asym)
+B_asym = -np.matmul(np.linalg.inv(C1_asym), C3_asym)
+C_asym = np.array([[1,0,0,0], [0,1,0,0], [0,0,1,0], [0,0,0,1]])
+D_asym = np.array([[0,0],[0,0],[0,0],[0,0]])
 
-sys_sym = signal.StateSpace(A, B, C, D)
+sys_asym = signal.StateSpace(A_asym, B_asym, C_asym, D_asym)
 
-print (sys_sym)
+print (sys_asym)
 
 
 
