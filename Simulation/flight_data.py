cadab07e60d0ffe3087ca02c1118121cccd5006d
tommyvtran97 <48211985+tommyvtran97@users.noreply.github.com>
2019-03-19 17:15:59 +0100

    Update flight_data.py

--- Simulation/flight_data.py
+++ Simulation/flight_data.py
@@ -2,14 +2,21 @@ from math import *
 import matplotlib.pyplot as plt
 
 
-time_d = [0]
-V_tas_d = [0]
-V_cas_d = [0]
-roll_angle_d = [0]
-pitch_angle_d = [0]
-body_roll_rate_d = [0]
-body_pitch_rate_d = [0]
-body_yaw_rate_d = [0]
+time_d1 = []
+V_tas_d1 = []
+V_cas_d1 = []
+roll_angle_d1 = []
+pitch_angle_d1 = []
+body_roll_rate_d1 = []
+body_pitch_rate_d1 = []
+body_yaw_rate_d1 = []
+fuel_right_d1 = []
+fuel_left_d1 = []
+delta_a_d1 = []
+delta_e_d1 = []
+delta_r_d1 = []
+hp_d1 = []
+lst_element = [27380, 28010, 28510, 30540, 31140, 32710]
 
 "*** Time ***"
 filename = 'time.dat' 
@@ -27,7 +34,7 @@ lines = V_tas_data.readlines()
 
 for line in lines:
     line1 = line.strip('  ').split('  ')
-    V_tas_d.append(line1[0])
+    V_tas_d1.append(line1[0])
 
 "*** Calibrated Airspeed ***"
 filename = 'V_cas.dat' 
@@ -36,7 +43,7 @@ lines = V_cas_data.readlines()
 
 for line in lines:
     line1 = line.strip('  ').split('  ')
-    V_cas_d.append(line1[0])
+    V_cas_d1.append(line1[0])
 
 "*** Roll Angle ***"
 filename = 'roll_angle.dat' 
@@ -45,7 +52,7 @@ lines = roll_angle_data.readlines()
 
 for line in lines:
     line1 = line.strip('  ').split('  ')
-    roll_angle_d.append(line1[0])
+    roll_angle_d1.append(line1[0])
 
 "*** Pitch Angle ***"
 filename = 'pitch_angle.dat' 
@@ -54,8 +61,86 @@ lines = pitch_angle_data.readlines()
 
 for line in lines:
     line1 = line.strip('  ').split('  ')
-    pitch_angle_d.append(line1[0])
+    pitch_angle_d1.append(line1[0])
 
+"*** Body Roll Rate ***"
+filename = 'body_roll_rate.dat' 
+body_roll_rate_data = open(filename,'r')
+lines = body_roll_rate_data.readlines()
 
-        
+for line in lines:
+    line1 = line.strip('  ').split('  ')
+    body_roll_rate_d1.append(line1[0])
+
+"*** Body Pitch Rate ***"
+filename = 'body_pitch_rate.dat' 
+body_pitch_rate_data = open(filename,'r')
+lines = body_pitch_rate_data.readlines()
+
+for line in lines:
+    line1 = line.strip('  ').split('  ')
+    body_pitch_rate_d1.append(line1[0])
+
+"*** Fuel Right Engine ***"
+filename = 'fuel_right.dat' 
+fuel_right_data = open(filename,'r')
+lines = fuel_right_data.readlines()
+
+for line in lines:
+    line1 = line.strip('  ').split('  ')
+    fuel_right_d1.append(line1[0])
+    
+"*** Fuel Left Engine ***"
+filename = 'fuel_left.dat' 
+fuel_left_data = open(filename,'r')
+lines = fuel_left_data.readlines()
+
+for line in lines:
+    line1 = line.strip('  ').split('  ')
+    fuel_left_d1.append(line1[0])
+
+"*** deflection aileron (right wing?) ***"
+filename = 'delta_a.dat' 
+delta_a_data = open(filename,'r')
+lines = delta_a_data.readlines()
+
+for line in lines:
+    line1 = line.strip('  ').split('  ')
+    delta_a_d1.append(line1[0])
+
+"*** deflection elevator ***"
+filename = 'delta_e.dat' 
+delta_e_data = open(filename,'r')
+lines = delta_e_data.readlines()
+
+for line in lines:
+    line1 = line.strip('  ').split('  ')
+    delta_e_d1.append(line1[0])
+
+"*** deflection rudder ***"
+filename = 'delta_r.dat' 
+delta_r_data = open(filename,'r')
+lines = delta_r_data.readlines()
+
+for line in lines:
+    line1 = line.strip('  ').split('  ')
+    delta_r_d1.append(line1[0])
+    
+
+"*** altitude ***"
+filename = 'altitude.dat' 
+altitude_data = open(filename,'r')
+lines = altitude_data.readlines()
+
+for line in lines:
+    line1 = line.strip('  ').split('  ')
+    hp_d1.append(line1[0])
+
+time_d = []
+for i in lst_element:
+    time_d.append(time_d1[i])
+
+print (time_d)
+ 
+
 
